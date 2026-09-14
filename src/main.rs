acc2980a47103d42826204a5d7a67ce45326a167
emanumctavares <emanumctavares@gmail.com>
2026-09-14 18:26:07 -0300

    add min and max

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use crate::utils::min;
+use crate::utils::max;
+
+pub mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let integers: Vec<i32> = vec![4, 1, 2, 3];
+    
+    let max_integer: Option<i32> = max(&integers);
+    match max_integer {
+        None => println!("There is no maximum integer."),
+        Some(x) => println!("max({x})")
+    }
+
+    let min_integer: Option<i32> = min(&integers);
+    match min_integer {
+        None => print!("There is no minimum integer."),
+        Some(x) => print!("min({x})")
+    }
 }
