4753a861482e56a55521cb95063507a4ee7f04ca
emanumctavares <emanumctavares@gmail.com>
2026-09-15 10:07:26 -0300

    improve min and max

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,47 @@ use crate::utils::max;
 pub mod utils;
 
 fn main() {
-    let integers: Vec<i32> = vec![4, 1, 2, 3];
+
+    let inputs: Vec<Vec<i32>> = vec![
+        vec![1, 2, 3],
+        vec![4, 1, 2, 3],
+        vec![5, 2, 3],
+        vec![5, 4, 3, 2, 1],
+        vec![i32::MAX],
+        vec![i32::MIN],
+        vec![i32::MAX, i32::MIN],
+        vec![]
+    ];
+
+    let mut outputs : Vec<Option<i32>> = vec![];
+
+    for input in &inputs {
+        let max_integer: Option<i32> = max(&input);
+        outputs.push(max_integer);
     
-    let max_integer: Option<i32> = max(&integers);
-    match max_integer {
-        None => println!("There is no maximum integer."),
-        Some(x) => println!("max({x})")
     }
 
-    let min_integer: Option<i32> = min(&integers);
-    match min_integer {
-        None => print!("There is no minimum integer."),
-        Some(x) => print!("min({x})")
+    // printing max results
+    for output in &outputs {
+        match output {
+            None => println!("There is no maximum integer."),
+            Some(x) => println!("max({x})")
+        }
     }
+    outputs.clear();
+
+    for input in &inputs {
+        let max_integer: Option<i32> = min(&input);
+        outputs.push(max_integer);
+    
+    }
+
+    // printing min results
+    for output in &outputs {
+        match output {
+            None => println!("There is no minimum integer."),
+            Some(x) => println!("min({x})")
+        }
+    }
+    outputs.clear();
 }
