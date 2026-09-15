a6cf6a95d7b66886fb3b31e819e7ff57b2df0463
emanumctavares <emanumctavares@gmail.com>
2026-09-15 10:40:52 -0300

    remove display elements

--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,46 @@ fn main() {
 
     let inputs: Vec<Vec<i32>> = vec![
         vec![1, 2, 3],
+        vec![-1, -2, -3],
         vec![4, 1, 2, 3],
+        vec![-4, 1, 2, 3],
         vec![5, 2, 3],
+        vec![5, -2, 3],
         vec![5, 4, 3, 2, 1],
+        vec![-5, -4, -3, -2, -1],
         vec![i32::MAX],
         vec![i32::MIN],
+        vec![i32::MAX, i32::MIN],
         vec![i32::MAX, i32::MIN],
         vec![]
     ];
 
-    let mut outputs : Vec<Option<i32>> = vec![];
-
+    let mut outputs: Vec<String> = vec![];
     for input in &inputs {
         let max_integer: Option<i32> = max(&input);
-        outputs.push(max_integer);
-    
+        match max_integer {
+            Some(x) => outputs.push(format!("{x}")),
+            None => outputs.push("No maximum integers found.".to_string())
+        }
     }
 
     // printing max results
     for output in &outputs {
-        match output {
-            None => println!("There is no maximum integer."),
-            Some(x) => println!("max({x})")
-        }
+        println!("{output}");
     }
     outputs.clear();
 
     for input in &inputs {
-        let max_integer: Option<i32> = min(&input);        
-        outputs.push(max_integer);
+        let min_integer: Option<i32> = min(&input);        
+        match min_integer {
+            Some(x) => outputs.push(format!("{x}")),
+            None => outputs.push("No minimum integers found.".to_string())
+        }
     }
 
     // printing min results
     for output in &outputs {
-        match output {
-            None => println!("There is no minimum integer."),
-            Some(x) => println!("min({x})")
-        }
+        println!("{output}");
     }
     outputs.clear();
 }
