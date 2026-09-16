ba518b8efcd2e4f0a3cbdf21c78bff72755ffa29
emanumctavares <emanumctavares@gmail.com>
2026-09-16 16:57:28 -0300

    remove all references to vec from tests

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -15,71 +15,71 @@ mod tests {
 
     #[test]
     fn test_empty() {
-        let mut values: Vec<i32> = vec![];
+        let mut values: [i32; 0] = [];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![]);
+        assert_eq!(values, []);
     }
 
     #[test]
     fn test_single_element() {
-        let mut values = vec![42];
+        let mut values = [42];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![42]);
+        assert_eq!(values, [42]);
     }
 
     #[test]
     fn test_already_sorted() {
-        let mut values = vec![1, 2, 3, 4, 5];
+        let mut values = [1, 2, 3, 4, 5];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![1, 2, 3, 4, 5]);
+        assert_eq!(values, [1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn test_reverse_sorted() {
-        let mut values = vec![5, 4, 3, 2, 1];
+        let mut values = [5, 4, 3, 2, 1];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![1, 2, 3, 4, 5]);
+        assert_eq!(values, [1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn test_unsorted_mixed_values() {
-        let mut values = vec![9, 1, 5, 3, 7, 2, 8, 4, 6];
+        let mut values = [9, 1, 5, 3, 7, 2, 8, 4, 6];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(values, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 
     #[test]
     fn test_negative_numbers() {
-        let mut values = vec![-3, 0, -10, 7, -1, 2];
+        let mut values = [-3, 0, -10, 7, -1, 2];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![-10, -3, -1, 0, 2, 7]);
+        assert_eq!(values, [-10, -3, -1, 0, 2, 7]);
     }
 
     #[test]
     fn test_duplicate_values() {
-        let mut values = vec![4, 2, 2, 1, 3, 4, 1];
+        let mut values = [4, 2, 2, 1, 3, 4, 1];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![1, 1, 2, 2, 3, 4, 4]);
+        assert_eq!(values, [1, 1, 2, 2, 3, 4, 4]);
     }
 
     #[test]
     fn test_all_equal_values() {
-        let mut values = vec![7, 7, 7, 7, 7];
+        let mut values = [7, 7, 7, 7, 7];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![7, 7, 7, 7, 7]);
+        assert_eq!(values, [7, 7, 7, 7, 7]);
     }
 
     #[test]
     fn test_integer_boundaries() {
-        let mut values = vec![i32::MAX, i32::MIN, 0, -1, 1];
+        let mut values = [i32::MAX, i32::MIN, 0, -1, 1];
         bubble_sort(&mut values);
-        assert_eq!(values, vec![i32::MIN, -1, 0, 1, i32::MAX]);
+        assert_eq!(values, [i32::MIN, -1, 0, 1, i32::MAX]);
     }
 
     #[test]
     fn test_characters() {
-        let mut values = vec!['d', 'a', 'c', 'b'];
+        let mut values = ['d', 'a', 'c', 'b'];
         bubble_sort(&mut values);
-        assert_eq!(values, vec!['a', 'b', 'c', 'd']);
+        assert_eq!(values, ['a', 'b', 'c', 'd']);
     }
 }
