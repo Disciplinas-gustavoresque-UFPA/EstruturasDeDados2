7b624f75dee8cd24d437df22010b7cef3a059d2f
emanumctavares <emanumctavares@gmail.com>
2026-09-16 15:24:51 -0300

    remove array clone

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,20 +1,16 @@
-pub fn insertion_sort<T: Ord + Copy>(elements: &Vec<T>) -> Vec<T> {
-    let mut elements_copy = elements.clone();
-    
-    let len = elements_copy.len();
+pub fn insertion_sort<T: Ord + Copy>(elements: &mut Vec<T>) {
+    let len = elements.len();
     for i in 1..len {
         let mut j = i;
-        let num = elements_copy[i];
+        let num = elements[i];
 
-        while j > 0 && num < elements_copy[j - 1] {
-            elements_copy[j] = elements_copy[j - 1];
+        while j > 0 && num < elements[j - 1] {
+            elements[j] = elements[j - 1];
             j -= 1;
         }
 
-        elements_copy[j] = num;
+        elements[j] = num;
     }
-
-    return elements_copy;
 }
 
 #[cfg(test)]
@@ -23,70 +19,71 @@ mod tests {
 
     #[test]
     fn test_empty() {
-        let values: Vec<i32> = vec![];
-        assert_eq!(insertion_sort(&values), vec![]);
+        let mut values: Vec<i32> = vec![];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![]);
     }
 
     #[test]
     fn test_single_element() {
-        let values = vec![42];
-        assert_eq!(insertion_sort(&values), vec![42]);
+        let mut values = vec![42];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![42]);
     }
 
     #[test]
     fn test_already_sorted() {
-        let values = vec![1, 2, 3, 4, 5];
-        assert_eq!(insertion_sort(&values), vec![1, 2, 3, 4, 5]);
+        let mut values = vec![1, 2, 3, 4, 5];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn test_reverse_sorted() {
-        let values = vec![5, 4, 3, 2, 1];
-        assert_eq!(insertion_sort(&values), vec![1, 2, 3, 4, 5]);
+        let mut values = vec![5, 4, 3, 2, 1];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![1, 2, 3, 4, 5]);
     }
 
     #[test]
     fn test_unsorted_mixed_values() {
-        let values = vec![9, 1, 5, 3, 7, 2, 8, 4, 6];
-        assert_eq!(insertion_sort(&values), vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        let mut values = vec![9, 1, 5, 3, 7, 2, 8, 4, 6];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 
     #[test]
     fn test_negative_numbers() {
-        let values = vec![-3, 0, -10, 7, -1, 2];
-        assert_eq!(insertion_sort(&values), vec![-10, -3, -1, 0, 2, 7]);
+        let mut values = vec![-3, 0, -10, 7, -1, 2];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![-10, -3, -1, 0, 2, 7]);
     }
 
     #[test]
     fn test_duplicate_values() {
-        let values = vec![4, 2, 2, 1, 3, 4, 1];
-        assert_eq!(insertion_sort(&values), vec![1, 1, 2, 2, 3, 4, 4]);
+        let mut values = vec![4, 2, 2, 1, 3, 4, 1];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![1, 1, 2, 2, 3, 4, 4]);
     }
 
     #[test]
     fn test_all_equal_values() {
-        let values = vec![7, 7, 7, 7, 7];
-        assert_eq!(insertion_sort(&values), vec![7, 7, 7, 7, 7]);
+        let mut values = vec![7, 7, 7, 7, 7];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![7, 7, 7, 7, 7]);
     }
 
     #[test]
     fn test_integer_boundaries() {
-        let values = vec![i32::MAX, i32::MIN, 0, -1, 1];
-        assert_eq!(insertion_sort(&values), vec![i32::MIN, -1, 0, 1, i32::MAX]);
-    }
-
-    #[test]
-    fn test_keeps_original_vector_unchanged() {
-        let values = vec![3, 1, 2];
-        let sorted = insertion_sort(&values);
-
-        assert_eq!(values, vec![3, 1, 2]);
-        assert_eq!(sorted, vec![1, 2, 3]);
+        let mut values = vec![i32::MAX, i32::MIN, 0, -1, 1];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec![i32::MIN, -1, 0, 1, i32::MAX]);
     }
 
     #[test]
     fn test_characters() {
-        let values = vec!['d', 'a', 'c', 'b'];
-        assert_eq!(insertion_sort(&values), vec!['a', 'b', 'c', 'd']);
+        let mut values = vec!['d', 'a', 'c', 'b'];
+        insertion_sort(&mut values);
+        assert_eq!(values, vec!['a', 'b', 'c', 'd']);
     }
 }
