d2cef00e74841d4f5c11c7060733ba5889cb11f4
Tiago da Silva Guerreiro <tiago.guerreiro76@gmail.com>
2026-09-19 00:02:30 -0300

    feat: base para o algoritmo RC4
    Signed-off-by: Tiago da Silva Guerreiro <tiago.guerreiro76@gmail.com>

--- src/encryption/mod.rs
+++ src/encryption/mod.rs
@@ -1,5 +1,6 @@
 mod symmetric;
 
 pub use symmetric::caesar_cipher;
+pub use symmetric::rc4;
 pub use symmetric::vigenere_cipher;
 pub use symmetric::xor_cipher;
--- src/encryption/symmetric/mod.rs
+++ src/encryption/symmetric/mod.rs
@@ -1,9 +1,11 @@
 mod caesar;
+mod rc4;
 mod utils;
 mod vigenere;
 mod xor;
 
 pub use caesar::caesar_cipher;
+pub use rc4::rc4;
 pub use utils::shift_char;
 pub use vigenere::vigenere_cipher;
 pub use xor::xor_cipher;
